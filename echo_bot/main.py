b6a978838abfcd9b7f293bc7702f55235ce37e0e
Hoher2000 <costet86@mail.ru>
2024-05-28 22:58:02 +0300

    add photo echo to echo bot

--- echo_bot/main.py
+++ echo_bot/main.py
@@ -1,6 +1,9 @@
 from aiogram import Bot, Dispatcher
 from aiogram.filters import Command
 from aiogram.types import Message
+from aiogram.types import ContentType
+from aiogram import F
+
 
 # Вместо BOT TOKEN HERE нужно вставить токен вашего бота, полученный у @BotFather
 BOT_TOKEN = '6825404817:AAGlZ2jhBU-wcxFGi2xIq9JXDpC49MgM-SU'
@@ -11,25 +14,36 @@ dp = Dispatcher()
 
 
 # Этот хэндлер будет срабатывать на команду "/start"
-@dp.message(Command(commands=["start"]))
+# @dp.message(Command(commands=["start"])) можно с декоратором
 async def process_start_command(message: Message):
     await message.answer('Привет!\nМеня зовут Эхо-бот!\nНапиши мне что-нибудь')
 
 
 # Этот хэндлер будет срабатывать на команду "/help"
-@dp.message(Command(commands=['help']))
+# @dp.message(Command(commands=['help']))
 async def process_help_command(message: Message):
     await message.answer(
         'Напиши мне что-нибудь и в ответ '
         'я пришлю тебе твое сообщение'
     )
 
+# Этот хэндлер будет срабатывать на отправку боту фото
+async def send_photo_echo(message: Message):
+    print(message)
+    await message.reply_photo(message.photo[0].file_id)
+
 
 # Этот хэндлер будет срабатывать на любые ваши текстовые сообщения,
 # кроме команд "/start" и "/help"
-@dp.message()
+# @dp.message()
 async def send_echo(message: Message):
     await message.reply(text=message.text)
+
+
+dp.message.register(process_start_command, Command(commands=["start"]))
+dp.message.register(process_help_command, Command(commands=["help"]))
+dp.message.register(send_photo_echo, F.photo)
+dp.message.register(send_echo)
 
 
 if __name__ == '__main__':
