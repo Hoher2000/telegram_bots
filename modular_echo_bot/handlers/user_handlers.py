38241a6a81df9c3e50cc0df3257cdcd85df89093
Hoher2000 <costet86@mail.ru>
2024-08-06 23:19:29 +0300

    Added modular echo bot

---
+++ modular_echo_bot/handlers/user_handlers.py
@@ -0,0 +1,20 @@
+from aiogram.types import Message # type: ignore
+from aiogram.filters import Command, CommandStart # type: ignore
+from aiogram import Router # type: ignore
+from lexicon.lexicon import LEXICON_RU # type: ignore
+
+
+# Инициализируем роутер уровня модуля
+router = Router()
+
+
+# Этот хэндлер срабатывает на команду /start
+@router.message(CommandStart())
+async def process_start_command(message: Message):
+    await message.answer(text=LEXICON_RU['/start'])
+
+
+# Этот хэндлер срабатывает на команду /help
+@router.message(Command(commands='help'))
+async def process_help_command(message: Message):
+    await message.answer(text=LEXICON_RU['/help'])
